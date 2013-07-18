48da61f01960e2b4011e28744fd88553c16b14f9
Mehmet Emre <maemre2@gmail.com>
2013-07-18 14:22:48 +0300

    operations module added

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <string>
+#include "operations.h"
 #include "cli.h"
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
+    init_managers();
     CLI_init();
     
     // Oh! Minimalism, my favourite
