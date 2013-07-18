43276b1368d7c02b871553a9141a96e393792991
Mehmet Emre <maemre2@gmail.com>
2013-07-18 14:27:48 +0300

    comparator function defined

--- src/util.h
+++ src/util.h
@@ -8,4 +8,7 @@ std::vector<std::string> split(const std::string &s, char delim, bool skip_empti
 
 #define PAGE_SIZE 1024
 
+// A comparator that compares two integers
+typedef bool (*comparator)(int, int);
+
 #endif
