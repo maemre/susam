1beb29aa26573d7a2fdb468bb208c958e352e11e
Mehmet Emre <maemre2@gmail.com>
2013-07-19 05:41:26 +0300

    Added a more comprehensive test.

--- src/syscat_test.cpp
+++ src/syscat_test.cpp
@@ -10,23 +10,29 @@ void createTableTest()
 {
     cout << "Creating two tables" << endl;
     Table t;
-    t.isDeleted=false;
+    t.isDeleted = false;
     // Table 1
-    t.name="students";
-    t.pk_index=0;
+    t.name = "students";
+    t.pk_index = 0;
     t.fields.push_back(make_pair("student id", 10));
     t.fields.push_back(make_pair("name", 15));
     t.fields.push_back(make_pair("dept", 4));
     t.fields.push_back(make_pair("email", 100));
-    cout << "Result: " << syscat.createTable(t) << endl;
-
-    t.name="courses";
-    t.pk_index=0;
+    cout << "1. Result: " << syscat.createTable(t) << endl;
+    
+    t.name = "courses";
+    t.pk_index = 0;
     t.fields.clear();
     t.fields.push_back(make_pair("course id", 4));
     t.fields.push_back(make_pair("dept", 4));
     t.fields.push_back(make_pair("year", 4));
-    cout << "Result: " << syscat.createTable(t) << endl;
+    cout << "2. Result: " << syscat.createTable(t) << endl;
+}
+
+void deleteTableTest()
+{
+    cout << "Deleting table students" << endl;
+    syscat.deleteTable("students");
 }
 
 void listTablesTest()
@@ -35,17 +41,32 @@ void listTablesTest()
     syscat.listAllTables();
 }
 
+void comboTest()
+{
+    listTablesTest();
+    deleteTableTest();
+    listTablesTest();
+    createTableTest();
+    listTablesTest();
+    cout << "Deleting all tables" << endl;
+    syscat.deleteTable("courses");
+    syscat.deleteTable("students");
+    listTablesTest();
+    createTableTest();
+    listTablesTest();
+}
+
 int main(int argc, char **argv)
-{   
+{
     try {
         syscat.open();
-    } catch (runtime_error err) {
+    }
+    catch (runtime_error err) {
         cerr << "Error: " << err.what() << endl;
         return EXIT_FAILURE;
     }
     
-    createTableTest();
-    listTablesTest();
-
+    char c;
+    comboTest();
     return 0;
 }
